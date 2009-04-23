a6aaa7d248b31a73c1485aadb10f1e84decd0ee7
Pritesh Mehta <pritesh@phatforge.com>
2009-04-23 03:15:41 +0000

    Bugfix: add a space to the CPPFLAGS append this concated the extra options without a space on my sabayon box, causing an install failure. Don't believe it affects other people. (built with REE-20090421)

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -13,7 +13,7 @@ end
 if make_libtar and have_header('zlib.h') and have_library('z')
   have_header('bzlib.h')
   have_library('bz2')
-  $CPPFLAGS << '-Ilibtar/lib -Ilibtar/listhash'
+  $CPPFLAGS << ' -Ilibtar/lib -Ilibtar/listhash'
   $objs = %w(tarruby.o libtar/lib/libtar.a)
   create_makefile('tarruby')
 end
